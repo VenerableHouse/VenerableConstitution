911b1062b3a765a879f09922ed9aead7edcec79e
Alex Ryan <ialexryan@users.noreply.github.com>
2017-09-01 02:09:29 -0700

    Add comment explaining where GH_TOKEN is defined

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,6 +3,8 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
+  # This is the only interesting line in the whole script. GH_TOKEN is defined in the Travis settings for this repository.
+  # The value of GH_TOKEN is a Github Personal Access Token with write access to the RuddockWebsite repository.
   git clone https://${GH_TOKEN}@github.com/RuddockHouse/RuddockWebsite.git
 }
 
