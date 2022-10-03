977fefa93d0a419705df068144c52a436da77551
ReubenRosenberg <52803246+ReubenRosenberg@users.noreply.github.com>
2022-10-02 22:19:12 -0700

    Update push.sh

--- .travis/push.sh
+++ .travis/push.sh
@@ -4,13 +4,13 @@ setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
   # This is the only interesting line in the whole script. GH_TOKEN is defined in the Travis settings for this repository.
-  # The value of GH_TOKEN is a Github Personal Access Token with write access to the RuddockWebsite repository.
-  git clone https://${GH_TOKEN}@github.com/RuddockHouse/RuddockWebsite.git
+  # The value of GH_TOKEN is a Github Personal Access Token with write access to the VenerableWebsite repository.
+  git clone https://${GH_TOKEN}@github.com/VenerableHouse/VenerableWebsite.git
 }
 
 commit_constitution() {
-  cp _build/constitution.pdf RuddockWebsite/ruddock/static/constitution/
-  cd RuddockWebsite
+  cp _build/constitution.pdf VenerableWebsite/ruddock/static/constitution/
+  cd VenerableWebsite
   git add ruddock/static/constitution/constitution.pdf
   git commit --message "Update constitution.pdf. [Travis build: $TRAVIS_BUILD_NUMBER]"
 }
